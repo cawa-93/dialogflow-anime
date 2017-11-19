187d91c08e42d223418598d6004a1f3b842db80c
Alex Kozack <kozackunisoft@gmail.com>
2017-11-19 22:28:32 +0200

    tg mode

--- index.js
+++ index.js
@@ -41,31 +41,32 @@ function processV1Request (request, response) {
         displayText: AnswerFactory.getAnswer(q, animes).text,
         data: {
           telegram: {
+            text: AnswerFactory.getAnswer(q, animes).text,
             parse_mode: 'HTML',
           }
         }
       })
     },
-    'SearchQuery.more': async function () {
-      var q = new SearchQuery(parameters)
-      let params = q.toApi()
-      console.log(q)
-      params.page = (params.page || 1) + 1
-      const animes = await getAnimes(params)
-      sendResponse({
-        data: {
-          telegram: {
-            text: AnswerFactory.getAnswer(q, animes).text,
-            parse_mode: 'HTML',
-          },
-        },
-        contextOut: [{
-          name: 'searchquery',
-          lifespan: '5',
-          parameters: params,
-        }],
-      })
-    },
+    // 'SearchQuery.more': async function () {
+    //   var q = new SearchQuery(parameters)
+    //   let params = q.toApi()
+    //   console.log(q)
+    //   params.page = (params.page || 1) + 1
+    //   const animes = await getAnimes(params)
+    //   sendResponse({
+    //     data: {
+    //       telegram: {
+    //         text: AnswerFactory.getAnswer(q, animes).text,
+    //         parse_mode: 'HTML',
+    //       },
+    //     },
+    //     contextOut: [{
+    //       name: 'searchquery',
+    //       lifespan: '5',
+    //       parameters: params,
+    //     }],
+    //   })
+    // },
   };
 
   // console.log(action, parameters)
@@ -96,12 +97,10 @@ function processV1Request (request, response) {
       responseJson.speech = responseToUser.speech || responseToUser.displayText;
       responseJson.displayText = responseToUser.displayText || responseToUser.speech;
       // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
-      responseJson.data = responseToUser.data || {
-        telegram: {
-          text: responseJson.displayText,
-          parse_mode: 'HTML',
-        }
-      };
+      responseJson.data = responseToUser.data
+      // if (responseJson.data.telegram && !responseJson.data.telegram.text) {
+        
+      // }
       // Optional: add contexts (https://dialogflow.com/docs/contexts)
       responseJson.contextOut = responseToUser.contextOut;
 
